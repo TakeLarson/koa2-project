4a34ada3ca33b4c57a48c96ce56782058d077da9
bin <819079978@qq.com>
2026-01-09 00:33:36 +0800

    提交初始版本

---
+++ src/config/config.default.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+// const config = {
+//     app: {
+//         port: process.env.APP_PORT || 8000,
+//     },
+// }
+module.exports = process.env;
---
+++ src/main.js
@@ -0,0 +1,5 @@
+const { APP_PORT } = require('./config/config.default');
+const app = require('./app');
+app.listen(APP_PORT, () => {
+    console.log(`Server is running on http://localhost:${APP_PORT}`);
+});
